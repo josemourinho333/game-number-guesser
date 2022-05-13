653919ab8f515a40284bc893fed80ccb157eaa15
Phil Y <yoo.phil92@gmail.com>
2022-05-13 16:58:59 -0700

    Updates to most checks, almost functional

--- script.js
+++ script.js
@@ -1,46 +1,93 @@
-const startGame = () => {
+$('.start-button').click(function() {
   $('.start-container').css('display', 'none');
   $('.rules-container').css('display', 'block');
-}
+})
 
-const startGuess = () => {
+$('.start-guessing-button').click(function() {
   $('.rules-container').css('display', 'none');
   $('.guess-container').css('display', 'block');
-}
+  //once clicked, need to generate a number that will last until play again is clicke
+  let secretNumber = Math.floor(Math.random()*50);
+  let storedGuesses = [];
+  console.log(`Secret number: ${secretNumber}`);
 
-const submitGuess = () => {
-  $('.guess-container').css('display', 'none');
-  $('.answer-container').css('display', 'block');
-}
+  $('.guess-submit-button').click(function() {
+    const guess = $('.guess-input').val();
 
-const playAgain = () => {
-  $('.answer-container').css('display', 'none');
-  $('.start-container').css('display', 'block');
-}
+    if (storedGuesses.includes(guess)) {
+      storedGuesses.push(guess);
+      $('.guess-input').val('');
+      $('.guess-container').css('display','none');
+      $('.answer-container').css('display', 'block');
+      $('.answer-text').text('You already guessed this. Try again.');
+      $('.play-again-button').hide();
+      setTimeout(function() {
+        $('.guess-container').css('display','block');
+        $('.answer-container').css('display', 'none');
+        $('.answer-text').text('');
+        $('.play-again-button').show()
+      }, 3000)
+    }
+
+    if (guess === '') {
+      $('.guess-input').val('');
+      $('.guess-container').css('display','none');
+      $('.answer-container').css('display', 'block');
+      $('.answer-text').text('Not a number. Try again.');
+      $('.play-again-button').hide();
+      setTimeout(function() {
+        $('.guess-container').css('display','block');
+        $('.answer-container').css('display', 'none');
+        $('.answer-text').text('');
+        $('.play-again-button').show()
+      }, 3000)
+    }
+
+    if (guess > secretNumber && !storedGuesses.includes(guess)) {
+      storedGuesses.push(guess);
+      $('.guess-input').val('');
+      $('.guess-container').css('display','none');
+      $('.answer-container').css('display', 'block');
+      $('.answer-text').text('Your guess is too high. Try again.');
+      $('.play-again-button').hide();
+      setTimeout(function() {
+        $('.guess-container').css('display','block');
+        $('.answer-container').css('display', 'none');
+        $('.answer-text').text('');
+        $('.play-again-button').show()
+      }, 3000)
+    }
+
+    if (guess < secretNumber && !storedGuesses.includes(guess)) {
+      storedGuesses.push(guess);
+      $('.guess-input').val('');
+      $('.guess-container').css('display','none');
+      $('.answer-container').css('display', 'block');
+      $('.answer-text').text('Your guess is too low. Try again.');
+      $('.play-again-button').hide();
+      setTimeout(function() {
+        $('.guess-container').css('display','block');
+        $('.answer-container').css('display', 'none');
+        $('.answer-text').text('');
+        $('.play-again-button').show();
+      }, 3000)
+    }
+
+    if (parseInt(guess) === secretNumber) {
+      storedGuesses.push(guess);
+      $('.guess-input').val('');
+      $('.guess-container').css('display','none');
+      $('.answer-container').css('display', 'block');
+      $('.answer-text').text(`You guessed right. It took you ${storedGuesses.length} tries.`);
+
+      $('.play-again-button').click(function() {
+        window.location.reload();
+      });
+    }
+  });
+
+})
 
 
 
 
-
-// Handles buttons in the page for the game
-$('button').click(function(e) {
-  // Declaring variable set to the class name of the elemnt clicked
-  let buttonClass = this.className; 
-
-  // Tells which function to run depending on which button is clicked
-  if (buttonClass === 'start-button') {
-    console.log('startbutton clicked');
-    startGame();
-  } else if (buttonClass === 'start-guessing-button') {
-    console.log('start guessing button clicked');
-    startGuess();
-  } else if (buttonClass === 'guess-submit-button') {
-    console.log('guess submit clicked');
-    submitGuess();
-  } else if (buttonClass === 'play-again-button') {
-    console.log('play again clicked');
-    playAgain();
-  } else {
-    console.log('this button not added yet in the code: ' + buttonClass);
-  }
-});
